9c1c93ff93b3cd398a3a13eb243c123f4b4eda99
Marina8989 <marina.mill209@gmail.com>
2022-03-07 22:23:24 -0800

    react int

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const App = () =>  {
+  const [value, setValue] = useState('');
+  //const [list, setList] = useState([])
+  const listArr = ['Jono', 'Marina', 'Lashandra', 'Bob', 'Lucia', 'Sofia', 'Erik', 'Jerald', 'Mary', 'Brian', 'Bernard']
+  const newArr = []
+
+  const handleChange  = (e) => {
+     setValue(e.target.value)
+     const x = listArr.map(item => {
+       if(item.substring(0, e.target.value.length).toUpperCase() === e.target.value.toUpperCase()) {
+          console.log(item)
+       }
+     })
+  }
   return (
     <div className="container">
-      <input />
+      <input value={value} onChange={(e) => handleChange(e)}/>
     </div>
   );
 }

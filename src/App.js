c4a029bfab15477dddc30629d90d0ea54b5b9ab5
Marina8989 <marina.mill209@gmail.com>
2022-03-09 16:16:40 -0800

    react int

--- src/App.js
+++ src/App.js
@@ -2,36 +2,19 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = () =>  {
-  const [value, setValue] = useState('');
-  const [list, setList] = useState()
-  const listArr = ['Jono', 'Marina', 'Lashandra', 'Bob', 'Lucia', 'Sofia', 'Erik', 'Jerald', 'Mary', 'Brian', 'Bernard']
-  const newArr = []
+  const [value, setValue] = useState('')
 
-  const setInputValue = (e, val) => {
-    setValue(val)
+  const handleChange = (e) => {
+     setValue(e.target.value)
   }
-
-  const handleChange  = (e) => {
-     setValue(e.target.value) 
-     listArr.forEach(item => {
-       if(item.substring(0, e.target.value.length).toUpperCase() === e.target.value.toUpperCase()) {
-         newArr.push(item)
-         console.log('newArr', newArr)
-
-         setList(newArr.map((item, index) => <li key={index} onClick={(e) => setInputValue(e, item)}>{item}</li>))
-       }
-       if(e.target.value.length === 0) {
-         setList([])
-       }
-     })
-  }
- console.log('list', list)
+  console.log(value)
   return (
     <>
-    <div className="container">
-      <input value={value} onChange={(e) => handleChange(e)}/> <br/>
-    </div>
-    {list}
+     <div>
+       <input type="text" value={value} onChange={handleChange}/>
+       <button>submit</button>
+     </div>
+     {value}
     </>
   );
 }

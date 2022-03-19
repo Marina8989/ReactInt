3ff7774ede3c0415965fc3b75706b394c783abfc
Marina8989 <marina.mill209@gmail.com>
2022-03-18 22:57:37 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './index.css';
 
 const App = () => {
+    const [value, setValue] = useState("");
+    const [list, setList] = useState([]);
+    const [searchValue, setSearchValue] = useState("")
+    
+    const handleChange = (e) => {
+      setValue(e.target.value);
+    }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const item = {
+          id: `${Math.random() - Math.random()}`,
+          value,
+          complete: false
+      }
+      const newList = [...list, item];
+      setList(newList)
+      setValue("");
+    }
+    const handleToggle = (el) => {
+      const newItem = list.map(item => {
+          if(item.id === el.id){
+             el.complete = !el.complete;
+          }
+          return item;
+      })
+      setList(newItem);
+    }
+    const handleRemove = (el) => {
+       const newItem = list.filter(item => item.id !== el.id)
+       setList(newItem)
+    }
+    const handleSearchValue = (e) => {
+      setSearchValue(e.target.value);
+      const newList = list.filter(item => item.value.includes(searchValue))
+      setList(newList)
+    }
     return(
-        <div style={{textAlign: 'center'}}>
-         <h1>Create Account</h1>
-         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
-             <h1>o</h1>
-             <h1>o</h1>
-             <h1>o</h1>
-             <h1>o</h1>
-         </div>
-         <p>or use your email for registration:</p>
-         <input type="text" placeholder="Name" /> <br /><br />
-         <input type="email" placeholder="Email" /><br /><br />
-         <input type="password" placeholder="Password" /><br /><br />
-         <input type="checkbox" />
-         <span>I agree to the Terms and Privacy Policy</span><br /><br />
-         <button>Sign Up</button>
-         <button>Sign In</button>
+        <div>
+          <form onSubmit={handleSubmit}>
+              <input type="text" value={value} onChange={handleChange}/>
+          </form>
+           <input value={searchValue} onChange={handleSearchValue} />
+          <ul>
+              {list.map(item => {
+                  return ( 
+                      <li key={item.id} className={`${item.complete ? 'cyan' : ''}`}>
+                       {item.value}
+                      <button onClick={() => handleToggle(item)}>toggle</button>
+                      <button onClick={() => handleRemove(item)}>remove</button>
+                      </li>
+                  )
+              })}
+          </ul>
         </div> 
     )
 }

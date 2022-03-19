af2608395f63b79289218d2e517be9667ae69e06
Marina8989 <marina.mill209@gmail.com>
2022-03-18 21:22:23 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-function Header(props) {
-    return (
-       <>
-         <h3>Hello, {props.name}! How are you?</h3>
-         <h4>Did you know that your mama loves you a lotttt?</h4>
-         <h5>{props.number}</h5>
-       </>
-    )
-}
-
 const App = () => {
     return(
-        <>
-          <Header name='Jono' number='5'/>
-
-        </>
+        <div style={{textAlign: 'center'}}>
+         <h1>Create Account</h1>
+         <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+             <h1>o</h1>
+             <h1>o</h1>
+             <h1>o</h1>
+             <h1>o</h1>
+         </div>
+         <p>or use your email for registration:</p>
+         <input type="text" placeholder="Name" /> <br /><br />
+         <input type="email" placeholder="Email" /><br /><br />
+         <input type="password" placeholder="Password" /><br /><br />
+         <input type="checkbox" />
+         <span>I agree to the Terms and Privacy Policy</span><br /><br />
+         <button>Sign Up</button>
+         <button>Sign In</button>
+        </div> 
     )
 }
 

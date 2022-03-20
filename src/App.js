5dfd619c0dce2b4579934664a2ec38c797e627ff
Marina8989 <marina.mill209@gmail.com>
2022-03-20 14:33:51 -0700

    react

--- src/App.js
+++ src/App.js
@@ -4,63 +4,22 @@ import "./index.css";
 const App = () => {
   const [value, setValue] = useState("");
   const [list, setList] = useState([]);
-  const [searchValue, setSearchValue] = useState("")
 
   const handleChange = (e) => {
-   setValue(e.target.value);
+     setValue(e.target.value);
   }
   const handleSubmit = (e) => {
      e.preventDefault();
-     const item = {
-       id: `${Math.random() - Math.random()}`,
-       value,
-       complete: false,
-       priority: 0
+     const newItem = {
+        id: `${Math.random() - Math.random()}`,
+        value
      }
-     const newList = [...list, item];
-     setList(newList);
+     const newList = [...list, newItem]
+     setList(newList)
      setValue("");
-  }
-  const handleToggle = (el) => {
-    const newItem = list.map(item => {
-      if(item.id === el.id){
-         item.complete = !item.complete;
-      }
-      return item;
-    })
-    setList(newItem);
   }
   const handleRemove = (el) => {
     const newItem = list.filter(item => item.id !== el.id);
-    setList(newItem);
-  }
-  const handleSearch = (e) => {
-    setSearchValue(e.target.value);
-    const newItem = list.filter(item => item.value.includes(searchValue));
-    setList(newItem)
-  }
-  const handlePriority = (el) => {
-     console.log(el.priority)
-     console.log(el)
-     const newItem = list.map(item => {
-       if(item.id === el.id) {
-           item.priority += 1
-       }
-       return item;
-     })
-     setList(newItem)
-     console.log(el.priority)
-     console.log(el)
-  }
-  const handleSort = () => {
-    const newItem = list.sort((a, b) => {
-      if(a.priority < b.priority) {
-         console.log('a', a.priority)
-      }
-      if(a.priority > b.priority) {
-        console.log('b', b.priority)
-     }
-    })
     setList(newItem);
   }
   return (
@@ -68,15 +27,12 @@ const App = () => {
     <form onSubmit={handleSubmit}>
       <input value={value} onChange={handleChange}/>
     </form>
-      <input value={searchValue} onChange={handleSearch}/>
     <ul>
-      {list.map(item => <li key={item.id} className={`${item.complete ? 'cyan' : ''}`}>
-        <h5>{item.value} / <span onClick={() => handlePriority(item)}>priority: {item.priority}/</span></h5>
-        <button onClick={() => handleToggle(item)}>toggle</button>
-        <button onClick={() => handleRemove(item)}>remove</button>
-        </li>)}
+     {list.map(item => <li key={item.id}>
+       {item.value}
+       <button onClick={() => handleRemove(item)}>X</button>
+       </li>)}
     </ul>
-    <button onClick={handleSort}>sort</button>
   </div>
   )
 };

faceb26b0341288487d844138f2a530f1bbb8028
Marina8989 <marina.mill209@gmail.com>
2022-04-05 12:45:39 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-const App = () => {
+function App () {
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const item = {
+      id: `${Math.random() - Math.random()}`,
+      value,
+      complete: false
+    }
+    const newItem = [...list, item];
+    setList(newItem);
+    setValue("");
+  }
+
   return (
-  <div className="main">
-    <h3>Create Account</h3>
-    <div className="icons">
-      <div className="icon"></div>
-      <div className="icon"></div>
-      <div className="icon"></div>
-      <div className="icon"></div>
+    <div>
+      <h2>react app</h2>
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={handleChange} />
+      </form>
+      <ul>
+        {list.map(item => <li key={item.id}>{item.value}</li>)}
+      </ul>
     </div>
-    <h6>or use your email for registration</h6>
-    <div className="inputs">
-      <input type="text" placeholder="Name" /><br/>
-      <input type="email" placeholder="Email" /><br/>
-      <input type="number" placeholder="Password" /><br/>
-    </div>
-    <div className="ckeck-input">
-      <input type="checkbox" />
-      <h6> I agree to the Terms and Privacy Policy</h6>
-    </div>
-    <div className="buttons">
-      <button>Sign Up</button>
-      <button>Sign In</button>
-    </div>
-  </div>
   )
-};
+}
 
 export default App;

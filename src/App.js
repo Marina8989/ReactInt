00772b196e09cba80f8c8fecaf812f215160f568
Marina8989 <marina.mill209@gmail.com>
2022-04-05 13:49:13 -0700

    react

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./index.css";
 function App () {
   const [value, setValue] = useState("");
   const [list, setList] = useState([]);
+  const [searchValue, setSearchValue] = useState("")
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -21,14 +22,38 @@ function App () {
     setValue("");
   }
 
+  const handleToggle = (el) => {
+    console.log('hellooo')
+   const newItem = list.map(item => {
+     if(item.id === el.id) {
+       item.complete = !item.complete;
+       console.log(item.complete, item.id)
+     }
+     return item;
+   })
+   setList(newItem)
+  }
+  const handleRemove = (el) => {
+     const newItem = list.filter(item => item.id !== el.id)
+     setList(newItem)
+  }
+  const handleSearchValue = (e) => {
+    setSearchValue(e.target.value)
+    const newItem = list.filter(item => item.value.includes(searchValue)) 
+    setList(newItem)
+  }
   return (
     <div>
       <h2>react app</h2>
       <form onSubmit={handleSubmit}>
         <input value={value} onChange={handleChange} />
       </form>
+      <input value={searchValue} onChange={handleSearchValue} />
       <ul>
-        {list.map(item => <li key={item.id}>{item.value}</li>)}
+        {list.map(item => <li key={item.id}>
+           <span>{item.value}</span>
+           <button onClick={() => handleRemove(item)}>remove</button>
+        </li>)}
       </ul>
     </div>
   )

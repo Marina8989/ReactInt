2b788f202855525eab12064db905efc3272d76f1
Marina8989 <marina.mill209@gmail.com>
2022-04-29 10:34:52 -0700

    react

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -2,103 +2,95 @@ import React from 'react';
 import './index.css';
 
 const Item = (props) => {
+    return (
+        <li className={props.item.completed ? 'cyan' : ''}>
+            {props.item.value}
+            <button onClick={() => props.handleToggle(props.item)}>toggle</button>
+            <button onClick={() => props.handleRemove(props.item)}>remove</button>
+        </li>
+    )
+}
+
+const List = (props) => {
+    return (
+        <ul>
+            {props.list.map(item => <Item item={item} key={item.id} handleToggle={props.handleToggle} handleRemove={props.handleRemove} />)}
+        </ul>
+    )
+
+}
+
+const SearchValue = (props) => {
    return (
        <>
-         <li className= {props.item.completed ? 'cyan' : ''}>
-             {props.item.value}
-             <button onClick={() => props.handleToggle(props.item)}>toggle</button>
-             <button onClick={() => props.handleRemove(props.item)}>remove</button>
-         </li>
+         <input value={props.searchValue} onChange={props.handleSearchValue} />
        </>
    )
 }
 
-const List = (props) => {
-   return (
-       <ul>
-           {props.list.map(item => <Item 
-                                      item={item} 
-                                      key={item.id} 
-                                      handleToggle={props.handleToggle}
-                                      handleRemove={props.handleRemove}
-                                   />)}
-       </ul>
-   )
-}
-
-const SearchInput = (props) => {
-        return(
-            <>
-              <input value={props.searchValue} onChange={props.handleSearch} />
-            </>
-        )
-    }
-
 class Form extends React.Component{
     state = {
-        searchInput: ""
+        inputValue: ''
     }
     handleChange = (e) => {
-       this.setState({searchInput: e.target.value})
+      this.setState({inputValue: e.target.value})
     }
     handleSubmit = (e) => {
-       e.preventDefault();
-       const value = this.state.searchInput;
-       this.setState({searchInput: ''});
-       this.props.handleSubmit(value);
+        e.preventDefault();
+        const value = this.state.inputValue;
+        this.setState({inputValue: ''});
+        this.props.handleSubmit(value);
     }
     render(){
-        return(
+        return (
             <form onSubmit={this.handleSubmit}>
-                <input value={this.state.value} onChange={this.handleChange} />
+                <input value={this.state.inputValue} onChange={this.handleChange} />
             </form>
         )
     }
 }
 
 class Search extends React.Component{
-    state={
+    state = {
         list: [],
         searchValue: ''
     }
     handleSubmit = (value) => {
-        const item = {
-            id: `${Math.random() - Math.random()}`,
-            value,
-            completed: false
-        }
-        const newList = [...this.state.list, item];
-        this.setState({list: newList})
-    }
-    handleToggle = (el) => {
-       const newItem = this.state.list.map(item => {
-           if(item.id === el.id) {
-               item.completed = !item.completed;
-           }
-           return item;
-       })
+       const item = {
+        id: `${Math.random()-Math.random()}`,    
+        value,
+        completed: false
+       }
+       const newItem = [...this.state.list, item];
        this.setState({list: newItem})
     }
-    handleRemove = (el) => {
-      const newList = this.state.list.filter(item => item.id !== el.id);
-      this.setState({list: newList});
+    handleToggle = (el) => {
+      const newList = this.state.list.map(item => {
+          if(item.id === el.id){
+               item.completed = !item.completed
+          }
+          return item;
+      })
+      this.setState({list: newList})
     }
-    handleSearch = (e) => {
-      this.setState({searchValue: e.target.value})
+    handleRemove = (el) => {
+       const newItem = this.state.list.filter(item => item.id !== el.id);
+       this.setState({list: newItem})
+    }
+    handleSearchValue = (e) => {
+       this.setState({searchValue: e.target.value});
     }
     render(){
         const newList = this.state.list.filter(item => item.value.includes(this.state.searchValue))
         return(
             <>
-               <Form handleSubmit={this.handleSubmit} />
-               <SearchInput handleSearch={this.handleSearch} />
-               <List 
-                  list={newList}
-                  handleToggle={this.handleToggle}
-                  handleRemove={this.handleRemove}
-               />
+              <h3>Grocery List</h3>
+              <Form  handleSubmit={this.handleSubmit}/>
+              <SearchValue searchValue={this.state.searchValue} handleSearchValue={this.handleSearchValue}/>
+              <List list={newList} handleToggle={this.handleToggle} handleRemove={this.handleRemove} />
             </>
         )
     }
 }
+
 export default Search

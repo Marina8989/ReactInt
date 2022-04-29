3b73ea943ccf3b03b4a0f3c09a4b441c37685959
Marina8989 <marina.mill209@gmail.com>
2022-04-29 14:08:59 -0700

    react

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -2,17 +2,68 @@ import React from 'react';
 
 class Counter extends React.Component{
     state = {
-        counter: this.props.counterIncrement
+        list: [
+            {
+                id: 1,
+                counter: 5,
+                inrement: 1
+            },
+            {
+                id: 2,
+                counter: 10,
+                increment: 5
+            },
+            {
+                id: 3,
+                counter: 7,
+                increment: 7
+            },
+            {
+                id: 4,
+                counter: 1
+            }
+        ],
+        sorted: null
     }
-    handleIncrement = () => {
-        this.setState({ counter : this.state.counter + (this.props.increment || 1)})
+    handleIncrement = (el) => {
+        const newCounter = this.state.list.map(item => {
+            if(item.id === el.id) {
+                item.counter += item.increment || 1;
+            }
+            return item;
+        })
+        this.setState({list: newCounter});
+    }
+    handleSort = () => {
+        if(this.state.sorted === null) {
+           this.setState({sorted: true})
+        }
+        if(this.state.sorted === true) {
+           this.setState({sorted: false})
+        }
+        if(this.state.sorted === false) {
+           this.setState({sorted: null})
+        }
+
+        const newList = this.state.list.sort((a,b) => {
+            if(this.state.sorted === true) {
+                return a.counter - b.counter
+            }else if(this.state.sorted === false) {
+                return b.counter - a.counter;
+            }
+        })
+
+        this.setState({list: newList})
     }
     render(){
         return(
-           <>
-             {this.state.counter}
-             <button onClick={this.handleIncrement}>increment</button>
-           </>
+           <div>
+             {this.state.list.map(item => <div key={item.id}>
+                 {item.counter}
+                 <button onClick={() => this.handleIncrement(item)}>increment</button>
+             </div>)}
+             <button onClick={this.handleSort}>sorted</button>
+           </div>
         )
     }
 }
@@ -21,9 +72,7 @@ class Project extends React.Component{
     render(){
         return(
             <>
-             <Counter counterIncrement={5} increment={7}/>
-             <Counter counterIncrement={10} increment={1}/>
-             <Counter counterIncrement={5} />
+             <Counter />
             </>
         )
     }

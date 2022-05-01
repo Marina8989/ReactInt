6f39e9919b5296f528d85cd29ff75adca45f79b3
Marina8989 <marina.mill209@gmail.com>
2022-05-01 14:54:34 -0700

    react

--- src/components/DataOne/index.js
+++ src/components/DataOne/index.js
@@ -3,25 +3,46 @@ import axios from 'axios';
 
 class DataOne extends React.Component{
     state = {
-        user: [],
+        users: [],
         searchInput: '',
         isLoading: false,
         hasError: false
+    }
+    getUser = async (user) => {
+        this.setState({isLoading: true});
+        try{
+            const {data} = await axios(`https://api.github.com/users/${user}`);
+            const newUser = [...this.state.users, data];
+            this.setState({users: newUser, isLoading: false, hasError: false});
+        }catch(err){
+           this.setState({hasError: true, isLoading: false});
+        }
     }
     handleChange = (e) => {
       this.setState({searchInput: e.target.value});
     }
     handleSubmit = (e) => {
        e.preventDefault();
-       this.setState({searchInput: ''})
+       this.getUser(this.state.searchInput);
+       this.setState({searchInput: ''});
     }
     render() {
-        console.log(this.state.searchInput)
+        console.log(this.state.users)
         return (
             <>
               <form onSubmit={this.handleSubmit}>
                   <input value={this.state.searchInput} onChange={this.handleChange} />
               </form>
+              {this.state.hasError && <h3>There was an error...</h3>}
+              {this.state.isLoading ? <h4>Loading...</h4> : (this.state.users.map(user => {
+                  return (
+                    <div key={user.id}>
+                        <h4>{user.login}</h4>
+                        <h4>Name: {user.name}</h4>
+                    </div>
+                  )
+              }))
+             }
             </>
         )
     }
